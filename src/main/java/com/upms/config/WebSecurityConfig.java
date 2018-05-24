cd696574fadec98b2c67c4a4da3b29bf4774d866
wuyao1994 <wuyaoiot@163.com>
2018-05-25 01:12:56 +0800

    complete step1

--- src/main/java/com/upms/config/WebSecurityConfig.java
+++ src/main/java/com/upms/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/home")
                 .permitAll()
-                .antMatchers("/user/**", "/users/**").hasAuthority("ADMIN")
+                .antMatchers("/user/**", "/users/**", "/book/**").hasAuthority("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/upms/controller/BookController.java
+++ src/main/java/com/upms/controller/BookController.java
@@ -3,6 +3,7 @@ package com.upms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +24,14 @@ public class BookController {
 		return bookService.findAll();
 	}
 
-
+    @PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createBook(@ModelAttribute Book book, Model model) {
 		bookService.insertBook(book);
         return "redirect:/book/all";
 	}
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addBook(Model model) {
         return "newBook";
@@ -43,25 +45,28 @@ public class BookController {
 	}
 
 
-
+    @PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 		bookService.deleteBook(id);
 		return "redirect:/book/all";
 	}
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
     public String updateBookPage(Model model, @PathVariable Long id) {
 	    model.addAttribute("book", bookService.findById(id));
 	    return "updateBook";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String updateBook(@ModelAttribute Book book, Model model) {
 		bookService.updateBook(book);
 		return "redirect:/book/all";
 	}
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public String allBook(Model model) {
 	    model.addAttribute("books", bookService.findAll());
