6b435d120ea2bed49eb9bfb04af6fc11422cb2f8
wuyao1994 <wuyaoiot@163.com>
2018-05-26 00:38:19 +0800

    ui

--- src/main/java/com/upms/controller/BookController.java
+++ src/main/java/com/upms/controller/BookController.java
@@ -18,58 +18,71 @@ public class BookController {
 	private BookService bookService;
 
 
-    @ResponseBody
+
+	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Book> getBookList() {
 		return bookService.findAll();
 	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+
+
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createBook(@ModelAttribute Book book, Model model) {
 		bookService.insertBook(book);
-        return "redirect:/book/all";
+		return "redirect:/book/all";
 	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String addBook(Model model) {
-        return "newBook";
-    }
 
 
-    @ResponseBody
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addBook(Model model) {
+		return "newBook";
+	}
+
+
+
+	@ResponseBody
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Book findBookById(@PathVariable("id") Long id) {
 		return bookService.findById(id);
 	}
 
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 		bookService.deleteBook(id);
 		return "redirect:/book/all";
 	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
-    public String updateBookPage(Model model, @PathVariable Long id) {
-	    model.addAttribute("book", bookService.findById(id));
-	    return "updateBook";
-    }
 
-    @PreAuthorize("hasAuthority('ADMIN')")
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
+	public String updateBookPage(Model model, @PathVariable Long id) {
+		model.addAttribute("book", bookService.findById(id));
+		return "updateBook";
+	}
+
+
+
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String updateBook(@ModelAttribute Book book, Model model) {
 		bookService.updateBook(book);
 		return "redirect:/book/all";
 	}
 
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public String allBook(Model model) {
-	    model.addAttribute("books", bookService.findAll());
-	    return "books";
-    }
+
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public String allBook(Model model) {
+		model.addAttribute("books", bookService.findAll());
+		return "books";
+	}
 }
